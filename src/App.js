a69fb5ae92d98874d59e7c716f3b9776e2965413
Jonas Gabriel <jgalmeida1993@gmail.com>
2021-07-19 22:20:56 -0300

    feat(initial commit): first config

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state/index'
+
+function App() {
+  const state = useSelector(state => state.account);
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch);
+
+
+  return (
+    <div className="App">
+      <h1>{state}</h1>
+      <button onClick={() => depositMoney(1000)}>Deposit</button>
+      <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
+    </div>
+  );
+}
+
+export default App;
